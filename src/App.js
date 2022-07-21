6c01c26366e9e795d0daa6cfae8c1d03ce9126ee
massimoalbarello <massimo00albarello@gmail.com>
2022-07-21 16:33:12 +0200

    detect gs1-128

--- src/App.js
+++ src/App.js
@@ -11,13 +11,31 @@ function App() {
     document.addEventListener('keyup', (event) => {
         if (tmpBarcode.length == 0) {
           setTimeout(() => {
-            if (tmpBarcode[0] === 'h') {
-              tmpBarcode = "https://" + tmpBarcode.slice(13).replaceAll('-', '/');
+            console.log(tmpBarcode);
+            let identifier;
+            if (tmpBarcode.slice(0, 4) === "http") {
+                identifier = "https://" + tmpBarcode.slice(13).replaceAll('-', '/');
             }
             else {
-              tmpBarcode = "https://dlnkd.tn.gg/01/" + tmpBarcode;
+              identifier = "https://dlnkd.tn.gg/";
+              let foundGS1_128 = false;
+              const startIndex = tmpBarcode.indexOf("01");
+              if (startIndex !== -1) {
+                const remainingId = tmpBarcode.slice(startIndex+2);
+                if (remainingId.length >= 14) {
+                  foundGS1_128 = true;
+                  identifier = identifier + ("01/" + remainingId.slice(0, 14) + "/");
+                  tmpBarcode = remainingId.slice(14);
+                  if (tmpBarcode.slice(0, 2) === "21") {
+                    identifier = identifier + "21/" + tmpBarcode.slice(2);
+                  }
+                }
+              }
+              if (!foundGS1_128) {
+                identifier += "01/" + tmpBarcode;
+              }
             }
-            barcodeContext.setBarcode(tmpBarcode);
+            barcodeContext.setBarcode(identifier);
             console.log("Scan detected");
             tmpBarcode = "";
           }, 500);
